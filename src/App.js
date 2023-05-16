cbe38d39f030f3582640749ad2692220dae79174
JKenslow <jkenslow@ksu.edu>
2023-05-16 14:23:36 -0500

    removed more default files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>hello world</h1>
+      {/* Sidebar */}
+      <Sidebar />
+      {/* Chat */}
     </div>
   );
 }
